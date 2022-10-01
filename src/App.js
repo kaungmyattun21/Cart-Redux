88c904b49991688720482d684eb93d5f4f0c98ca
Kaung Myat Tun <kaungtun111@gmail.com>
2022-10-02 00:43:31 +0630

    first action

--- src/App.js
+++ src/App.js
@@ -5,13 +5,33 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import { createStore } from 'redux'
+// initial store
+const initialStore = {
+  count: 0
+}
+
+//reducer
+const reducer = (state, action) => {
+  console.log({state , action});
+  if (action.type === 'DECREASE') {
+    return {count: state.count - 1}
+  }
+  return state
+}
+
+const store = createStore(reducer, initialStore)
+store.dispatch({type: 'DECREASE'})
+store.dispatch({type: 'INCREASE'})
+store.dispatch({type: 'INCREASE'})
+store.dispatch({type: 'RESET'})
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
