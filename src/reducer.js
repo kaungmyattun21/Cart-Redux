f84b20aedf8bb85d8bf62db10fd143f773e63e03
Kaung Myat Tun <kaungtun111@gmail.com>
2022-10-04 00:39:56 +0630

    complete item remove

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,24 @@
-import { DECREASE, INCREASE, CLEAR_CART } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions";
 
 const reducer = (state, action) => {
-  if (action.type === CLEAR_CART) {
-    return {...state, cart: [] }
+  switch (action.type) {
+    case CLEAR_CART:
+      return {...state, cart: []}
+      break;
+    case DECREASE:
+      console.log('u decrease amount');
+      break
+    case INCREASE:
+      console.log('u increase  amount');
+      break
+    case REMOVE:
+      return {...state, cart: state.cart.filter(cartItem => cartItem.id !== action.payload.id)}
+      break
+    default:
+      
   }
   return state
+  
 }
 
 export default reducer
