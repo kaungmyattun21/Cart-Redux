49cc8c19f038e3f7d7471a1c40ba4140b0b360b1
Kaung Myat Tun <kaungtun111@gmail.com>
2023-01-19 15:01:02 +0630

    editing user interface

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { INCREASE, DECREASE, REMOVE } from "../actions";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 const CartItem = ({ img, title, price, amount, remove, increase, decrease }) => {
   return (
@@ -12,22 +14,19 @@ const CartItem = ({ img, title, price, amount, remove, increase, decrease }) => 
         {/* remove button */}
         <button className="remove-btn" onClick={() => remove()}>remove</button>
       </div>
-      <div>
+      <div className="cart-item-container">
         {/* increase amount */}
         <button className="amount-btn" onClick={() => increase()}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z" />
-          </svg>
+          <AddIcon />
         </button>
         {/* amount */}
         <p className="amount">{amount}</p>
         {/* decrease amount */}
         <button className="amount-btn" onClick={() => decrease()}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-          </svg>
+          <RemoveIcon />
         </button>
       </div>
+      
     </div>
   );
 };
